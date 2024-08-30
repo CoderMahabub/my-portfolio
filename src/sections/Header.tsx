158e16bdec76ecbafdf11507cf7212a303269879
CoderMahabub <CoderMahabub@gmail.com>
2024-08-30 23:54:59 +0900

    Framer motion to drag

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -3,17 +3,17 @@ export const Header = () => {
     <div className="flex justify-center items-center fixed top-3 left-1/2 transform -translate-x-1/2 z-10">
       {/* <div className="flex justify-center items-center fixed top-3"> */}
       <nav className="flex gap-1 p-0.5 border border-white/15 bg-white/10 backdrop-blur  rounded-full">
-        <a href="#" className="nav-item">
+        <a href="#home" className="nav-item">
           Home
         </a>
-        <a href="#" className="nav-item">
+        <a href="#projects" className="nav-item">
           Projects
         </a>
-        <a href="#" className="nav-item">
+        <a href="#about" className="nav-item">
           About
         </a>
         <a
-          href="#"
+          href="#contact"
           className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
         >
           Contact
